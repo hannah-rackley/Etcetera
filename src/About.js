671ca7a6bfbce782294b05762377283ac06b993d
Hannah <hrglasser94@gmail.com>
2018-09-20 15:04:37 -0400

    Moved about to its own componenet

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SideBar from './sidebar.js';
+import NavBar from './Navigation';
+
+let About = props => <div>
+    <NavBar/>
+    <SideBar/>
+        <h1>ABOUT</h1>
+    </div>
+
+export default About;
